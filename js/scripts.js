c357353f7fe5f6c5a13c5e0943519164f566ee07
Matthew Terrill <meterrill@gmail.com>
2015-02-11 10:27:08 -0800

    add webpage interface for change calculator

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,18 @@ var coinCombos = function(cents) {
   var pennies = Math.floor(cents - (25 * quarters) - (10 * dimes) - (5 * nickels));
   return [quarters, dimes, nickels, pennies];
 };
+
+$(document).ready(function() {
+  $("form#coins").submit(function(event) {
+    var change = parseInt($("input#change").val());
+    var changeArray = coinCombos(change);
+
+    $(".quarters").text(changeArray[0]);
+    $(".dimes").text(changeArray[1]);
+    $(".nickels").text(changeArray[2]);
+    $(".pennies").text(changeArray[3]);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
